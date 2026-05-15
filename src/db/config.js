7b463433cbd441a9063a44f2c423567057f9b2df
Katherine Vasquez <kathevasquez@gmail.com>
2026-05-15 14:32:24 -0500

    fix local and production postgres config

--- src/db/config.js
+++ src/db/config.js
@@ -1,12 +1,23 @@
 const { Pool } = require("pg");
+require("dotenv").config();
 
-const isProduction = process.env.DATABASE_URL;
+const isProduction = process.env.DATABASE_URL?.includes("railway");
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: isProduction
-    ? { rejectUnauthorized: false }
-    : false
-});
+const pool = new Pool(
+  isProduction
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false
+        }
+      }
+    : {
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT
+      }
+);
 
 module.exports = pool;
