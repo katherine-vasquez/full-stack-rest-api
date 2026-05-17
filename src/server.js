8d527367ae6227760dbd53439bb0383feeb29b65
Katherine Vasquez <kathevasquez@gmail.com>
2026-05-17 08:39:40 -0500

    add root route

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+
 
 const app = require("./app");
 const pool = require("./db/config");
@@ -8,6 +8,10 @@ const PORT = process.env.PORT || 3000;
 pool.connect()
   .then(() => console.log("🟢 PostgreSQL conectado correctamente"))
   .catch((err) => console.error("🔴 Error DB:", err));
+
+app.get("/", (req, res) => {
+  res.send("🚀 API funcionando correctamente");
+});
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
