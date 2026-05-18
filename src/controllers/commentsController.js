0dac23f2d3c2525f24e002039d20d8474a47d84f
Katherine Vasquez <kathevasquez@gmail.com>
2026-05-18 15:15:19 -0500

    Restore comments controller clean version

--- src/controllers/commentsController.js
+++ src/controllers/commentsController.js
@@ -1,17 +1,58 @@
 const commentsService = require("../services/commentsServices");
+console.log(commentsService);
 
-console.log("SERVICE LOADED:", Object.keys(commentsService));
-
+// GET ALL COMMENTS
 const getAllComments = async (req, res, next) => {
   try {
     const comments = await commentsService.getAllComments();
-    res.status(200).json(comments);
+
+    return res.status(200).json(comments);
+
   } catch (error) {
-    console.log(error);
+    next(error);
+  }
+};
+
+// GET COMMENTS BY POST
+const getCommentsByPost = async (req, res, next) => {
+  try {
+    const { postId } = req.params;
+
+    const comments = await commentsService.getCommentsByPost(postId);
+
+    return res.status(200).json(comments);
+
+  } catch (error) {
+    next(error);
+  }
+};
+
+// CREATE COMMENT
+const createComment = async (req, res, next) => {
+  try {
+    const { post_id, author_id, content } = req.body;
+
+    if (!post_id || !author_id || !content) {
+      return res.status(400).json({
+        message: "post_id, author_id y content son obligatorios"
+      });
+    }
+
+    const newComment = await commentsService.createComment({
+      post_id,
+      author_id,
+      content
+    });
+
+    return res.status(201).json(newComment);
+
+  } catch (error) {
     next(error);
   }
 };
 
 module.exports = {
-  getAllComments
+  getAllComments,
+  getCommentsByPost,
+  createComment
 };
