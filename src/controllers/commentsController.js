fadc4faf7c888218f34954663069dbbd41f1fd0c
Katherine Vasquez <kathevasquez@gmail.com>
2026-05-18 15:07:14 -0500

    Debug comments service export issue

--- src/controllers/commentsController.js
+++ src/controllers/commentsController.js
@@ -1,58 +1,17 @@
 const commentsService = require("../services/commentsServices");
-console.log(commentsService);
 
-// GET ALL COMMENTS
+console.log("SERVICE LOADED:", Object.keys(commentsService));
+
 const getAllComments = async (req, res, next) => {
   try {
     const comments = await commentsService.getAllComments();
-
-    return res.status(200).json(comments);
-
+    res.status(200).json(comments);
   } catch (error) {
-    next(error);
-  }
-};
-
-// GET COMMENTS BY POST
-const getCommentsByPost = async (req, res, next) => {
-  try {
-    const { postId } = req.params;
-
-    const comments = await commentsService.getCommentsByPost(postId);
-
-    return res.status(200).json(comments);
-
-  } catch (error) {
-    next(error);
-  }
-};
-
-// CREATE COMMENT
-const createComment = async (req, res, next) => {
-  try {
-    const { post_id, author_id, content } = req.body;
-
-    if (!post_id || !author_id || !content) {
-      return res.status(400).json({
-        message: "post_id, author_id y content son obligatorios"
-      });
-    }
-
-    const newComment = await commentsService.createComment({
-      post_id,
-      author_id,
-      content
-    });
-
-    return res.status(201).json(newComment);
-
-  } catch (error) {
+    console.log(error);
     next(error);
   }
 };
 
 module.exports = {
-  getAllComments,
-  getCommentsByPost,
-  createComment
+  getAllComments
 };
