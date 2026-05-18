1fba88199b106cdad6c96154bfc541d8d106594d
Katherine Vasquez <kathevasquez@gmail.com>
2026-05-18 14:23:30 -0500

    Fix comments router

---
+++ src/routes/commentsRouter.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const router = express.Router();
+
+const {
+  getAllComments,
+  getCommentsByPost,
+  createComment
+} = require("../controllers/commentsController");
+
+// GET ALL COMMENTS
+router.get("/", getAllComments);
+
+// GET COMMENTS BY POST
+router.get("/post/:postId", getCommentsByPost);
+
+// CREATE COMMENT
+router.post("/", createComment);
+
+module.exports = router;
